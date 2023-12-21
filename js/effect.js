a7cd6d4cc71489454956de5426b1da115ccccfa8
MaryLishha <122745935+MaryLishha@users.noreply.github.com>
2023-12-21 18:57:16 +0300

    header_theme

--- js/effect.js
+++ js/effect.js
@@ -1,7 +1,13 @@
 
-let container = document.querySelector('.header__container')
-let headerMenu = document.querySelector('.header__menu')
+const container = document.querySelector('.header__container')
+const headerMenu = document.querySelector('.header__menu')
 container.onclick = function () {
     container.classList.toggle("header_change");
     headerMenu.classList.toggle('header__menu_hidden')
+}
+const theme = document.querySelector('.header__theme') 
+theme.onclick = () => {
+let themeIcon = document.querySelector('.header__theme_icon') 
+    themeIcon.classList.toggle('header__theme_active_float')
+    theme.classList.toggle('')
 }
